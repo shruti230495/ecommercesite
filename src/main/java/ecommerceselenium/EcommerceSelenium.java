e6786bd9b4d787f2ab20fc32a3574509c81d4730
shruti230495 <shrutib2395@gmail.com>
2025-08-21 10:55:39 -0700

    1st Commit includes maximizing the window ,launching the browser and site

--- src/main/java/ecommerceselenium/EcommerceSelenium.java
+++ src/main/java/ecommerceselenium/EcommerceSelenium.java
@@ -1,4 +1,16 @@
 package ecommerceselenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class EcommerceSelenium {
+    public static void main(String[] args){
+        //maximize the window using ChromeOptions
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        //launch the browser and ecommerce site
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://automationpractice.pl");
+    }
 }
